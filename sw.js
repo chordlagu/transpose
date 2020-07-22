77b07705ec351458419d69d83b7a1b5310b1ac44
chordlagu <68489644+chordlagu@users.noreply.github.com>
2020-07-23 04:58:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
 var cacheName = 'hello-pwa'; 
 var filesToCache = [
 '/',    
-'/index.html',    
+'/index.html',
+'https://1.bp.blogspot.com/-OzWSvdMBej8/XxSa5nQN7_I/AAAAAAAAEgw/LLqI8Qhavc0FrTVaWHpdMtICSdCOax7OACPcBGAYYCw/s1600/CHORDLAGU_ID%2B%25281%2529%2B%25281%2529.png',
+'/meter.js',
+'/notes.js',
+'/tuner.js',
+'/app.js',
+'/frequency-bars.js',
+'/aubio.js',
+'/aubio.wasm',
+'/index.html',
 '/style.css',  
 '/main.js'  ];  
 self.addEventListener('install', function(e) { 
